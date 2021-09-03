ae9c3ba7cc171586fb08994a684ceacc1e6c6841
oysa88 <oystein.salomonsen@tekniskmuseum.no>
2021-09-03 23:54:57 +0200

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     activateLights()
 })
-function win() {
+function win () {
     radio.sendString("S")
 }
 radio.onReceivedString(function (receivedString) {
@@ -15,26 +15,26 @@ radio.onReceivedString(function (receivedString) {
         control.reset()
     }
 })
-function activateLights() {
+function activateLights () {
     lightsOn = !(lightsOn)
     lightsOnTime = input.runningTime()
     pins.digitalWritePin(lightsPin, 1)
+led.toggleAll();
+basic.pause(onDuration)
     led.toggleAll();
-    basic.pause(onDuration)
-    led.toggleAll();
-    pins.digitalWritePin(lightsPin, 0)
+pins.digitalWritePin(lightsPin, 0)
 }
 let lightsOn = false
 let lightsOnTime = 0
 let onDuration = 0
-let turbineOnTime = 0
 let turbineIsRunning = false
+let turbineOnTime = 0
 radio.setGroup(6)
 basic.showIcon(IconNames.Happy)
 // 15 sec timeout
 let turbineTimeout = 15000
 let lightsOnTimeout = 10000
-onDuration = 100
+onDuration = 1100
 let lightsPin = DigitalPin.P0
 basic.forever(function () {
     if (lightsOn) {
